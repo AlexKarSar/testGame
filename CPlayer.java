b83ca083cd65f57797a98253699367dcda17c500
AlexKarSar <146998866+AlexKarSar@users.noreply.github.com>
2023-10-04 23:41:52 +0300

    CPlayer.java

--- CPlayer.java
+++ CPlayer.java
@@ -5,7 +5,8 @@ public class CPlayer extends CCreature implements ICreature.IPlayer {
     private static byte numOfHeal = 4;
     private int healthMax;
 
-    CPlayer(){}
+    CPlayer() {
+    }
 
     public CPlayer(byte attack, byte defence, int health, int damageN, int damageM) {
         super(attack, defence, health, damageN, damageM);
@@ -18,22 +19,22 @@ public class CPlayer extends CCreature implements ICreature.IPlayer {
 
     @Override
     public void heal() {
-        if(health == 0 || numOfHeal == 0) return;
-        if(healthMax - health > healthMax * 0.3)health += healthMax * 0.3;
+        if (health == 0 || numOfHeal == 0) return;
+        if (healthMax - health > healthMax * 0.3) health += healthMax * 0.3;
         else health = healthMax;
         System.out.printf("Игрок восстановил здоровье на %.0f, теперь оно равно %d\n", healthMax * 0.3 - 1, health);
         numOfHeal -= 1;
     }
 
-    public void getInfo(){
+    public void getInfo() {
         System.out.printf("Показатели Игрока :\tЗдоровье -> %d; Урон -> [%d;%d]; Атака -> %d; Защита -> %d; Осталось исцелений -> %d\n", getHealth(), getDamage()[0], getDamage()[1], getAttack(), getDefence(), numOfHeal);
     }
 
-    public byte getNumOfHeal(){
+    public byte getNumOfHeal() {
         return numOfHeal;
     }
 
-    public void setNumOfHeal(byte num){
+    public void setNumOfHeal(byte num) {
         numOfHeal = num;
     }
 }
